f32b36171e7c3c2f3ee6148007aa3bb21dd57161
ACA15JCH <jcdhan1@sheffield.ac.uk>
2018-11-19 17:55:23 +0000

    UI visible but non functional

--- Week8Exercise/app/src/main/java/myapplication/uk/ac/shef/oak/myapplication/MapsActivity.java
+++ Week8Exercise/app/src/main/java/myapplication/uk/ac/shef/oak/myapplication/MapsActivity.java
@@ -4,12 +4,17 @@
 
 package myapplication.uk.ac.shef.oak.myapplication;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.IntentService;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.icu.text.DateFormat;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
@@ -17,8 +22,10 @@ import android.widget.Button;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -26,173 +33,215 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-	private static final int ACCESS_FINE_LOCATION = 123;
-	private GoogleMap mMap;
-	private LocationRequest mLocationRequest;
-	private FusedLocationProviderClient mFusedLocationClient;
-	private MapView mapView;
+    private static Activity activity;
+    private static GoogleMap mMap;
+    private static final int ACCESS_FINE_LOCATION = 123;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private MapView mapView;
+    private Button mButtonStart;
+    private Button mButtonEnd;
+    private PendingIntent mLocationPendingIntent;
+
+    public static Activity getActivity() {
+        return activity;
+    }
+
+    public static void setActivity(Activity activity) {
+        MapsActivity.activity = activity;
+    }
+
+    public static GoogleMap getMap() {
+        return mMap;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setActivity(this);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mButtonStart = (Button) findViewById(R.id.button_start);
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLocationUpdates(getApplicationContext());
+                if (mButtonEnd != null)
+                    mButtonEnd.setEnabled(true);
+                mButtonStart.setEnabled(false);
+            }
+        });
+        mButtonStart.setEnabled(true);
+
+        mButtonEnd = (Button) findViewById(R.id.button_end);
+        mButtonEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopLocationUpdates();
+                if (mButtonStart != null)
+                    mButtonStart.setEnabled(true);
+                mButtonEnd.setEnabled(false);
+            }
+        });
+        mButtonEnd.setEnabled(false);
+        initLocations();
+    }
 
 
-	private LocationRequest mLocationRequest;
-	private FusedLocationProviderClient mFusedLocationClient;
+    private void initLocations() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-	/**
-	 * this is the location intent for the location tracking. It is an internal class of type IntentService
-	 * The intentservice will be invoked when the next location is available. The method called will be
-	 * onHandleIntent
-	 */
-	public static class LocationIntent extends IntentService {
-		public LocationIntent(String name) {
-			super(name);
-		}
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
 
-		public LocationIntent() {
-			super("Location Intent");
-		}
+            } else {
 
-		/**
-		 * called when a location is recognised
-		 *
-		 * @param intent
-		 */
-		@Override
-		protected void onHandleIntent(Intent intent) {
-			if (LocationResult.hasResult(intent)) {
-				LocationResult locResults = LocationResult.extractResult(intent);
-				if (locResults != null) {
-					for (Location location : locResults.getLocations()) {
-						if (location == null) continue;
-						//do something with the location
-						Log.i("New Location", "Current location: " + location);
-						mCurrentLocation = location;
-						mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-						Log.i("MAP", "new location " + mCurrentLocation.toString());
-						// check if the activity has not been closed in the meantime
-						if (getActivity() != null)
-							// any modification of the user interface must be done on the UI Thread.
-							// The Intent Service is running
-							// in its own thread, so it cannot communicate with the UI.
-							getActivity().runOnUiThread(new Runnable() {
-								public void run() {
-									Log.i("New Location", "Current location: "+location);
-								}
-							});
-					}
-				}
-			}
-		}
-	}
+                // No explanation needed, we can request the permission.
 
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        ACCESS_FINE_LOCATION);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_maps);
-		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
-		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.map);
-		mapFragment.getMapAsync(this);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
 
-		final Button mButtonStart = (Button) findViewById(R.id.button_start);
-		mButtonStart.setEnabled(true);
-		final Button mButtonEnd = (Button) findViewById(R.id.button_end);
-		mButtonEnd.setEnabled(false);
+            return;
+        }
+    }
 
-		mButtonStart.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				private void startLocationUpdates(){
-					Intent intent = new Intent(context, LocationIntent.class);
-					mLocationPendingIntent = PendingIntent.getService(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-					Task<Void> locationTask = mFusedLocationClient.requestLocationUpdates(mLocationRequest,
-							mLocationPendingIntent);
-					if (locationTask != null) {
-						locationTask.addOnFailureListener(new OnFailureListener() {
-							@Override
-							public void onFailure(@NonNull Exception e) {
-								if (e instanceof ApiException) {
-									Log.w(TAG, ((ApiException) e).getStatusMessage());
-								} else {
-									Log.w(TAG, e.getMessage());
-								}
-							}
-						});
-						locationTask.addOnCompleteListener(new OnCompleteListener<Void>() {
-							@Override
-							public void onComplete(@NonNull Task<Void> task) {
-								Log.d(TAG, "restarting gps successful!");
-							}
-						});
-					}
-				}
-				startLocationUpdates();
-				if
-						(mButtonEnd!=null)
-					mButtonEnd.setEnabled(true);
-				mButtonStart.setEnabled(false);
-			}
-		});
-		mButtonEnd.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if
-						(mButtonStart!=null)
-					mButtonStart.setEnabled(true);
-				mButtonEnd.setEnabled(false);
-			}
-		});
-	}
+    private void startLocationUpdates(Context context) {
+        Intent intent = new Intent(context, LocationIntent.class);
+        mLocationPendingIntent = PendingIntent.getService(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Task<Void> locationTask = mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                    mLocationPendingIntent);
+            if (locationTask != null) {
+                locationTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof ApiException) {
+                            Log.w("MapsActivity", ((ApiException) e).getStatusMessage());
+                        } else {
+                            Log.w("MapsActivity", e.getMessage());
+                        }
+                    }
+                });
+
+                locationTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("MapsActivity", "restarting gps successful!");
+                    }
+                });
 
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+            }
+        }
+    }
 
-	}
+    /**
+     * it stops the location updates
+     */
+    private void stopLocationUpdates(){
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        startLocationUpdates(this);
+    }
 
 
-	@SuppressLint("MissingPermission")
-	@Override
-	public void onRequestPermissionsResult(int requestCode,
-										   String permissions[], int[] grantResults) {
-		switch (requestCode) {
-			case ACCESS_FINE_LOCATION: {
-				// If request is cancelled, the result arrays are empty.
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            mCurrentLocation = locationResult.getLastLocation();
+            mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+            Log.i("MAP", "new location " + mCurrentLocation.toString());
+            if (mMap != null)
+                mMap.addMarker(new MarkerOptions().position(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
+                        .title(mLastUpdateTime));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()), 14.0f));
+        }
+    };
 
 
-				return;
-			}
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-			// other 'case' lines to check for other
-			// permissions this app might request
-		}
-	}
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                            mLocationCallback, null /* Looper */);
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
 
-	/**
-	 * Manipulates the map once available.
-	 * This callback is triggered when the map is ready to be used.
-	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
-	 * we just add a marker near Sydney, Australia.
-	 * If Google Play services is not installed on the device, the user will be prompted to install
-	 * it inside the SupportMapFragment. This method will only be triggered once the user has
-	 * installed Google Play services and returned to the app.
-	 */
-	@Override
-	public void onMapReady(GoogleMap googleMap) {
-		mMap = googleMap;
-		mMap.getUiSettings().setZoomControlsEnabled(true);
-		// Add a marker in Sydney and move the camera
-		LatLng sydney = new LatLng(-34, 151);
-		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14.0f));
 
-	}
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14.0f));
+
+    }
 }
-
